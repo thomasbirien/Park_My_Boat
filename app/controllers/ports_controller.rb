b6d290f7503c0d7eac98930f057000ac7c1b58ed
= <jbcousinat@gmail.com>
2017-02-28 18:15:24 +0100

    add geocoder

--- app/controllers/ports_controller.rb
+++ app/controllers/ports_controller.rb
@@ -1,4 +1,13 @@
 class PortsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+
   def index
+    @ports = Port.all
   end
+
 end
+
+
+
