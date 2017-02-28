55effa9f26de6b393972f1a3e074e97c97953343
Thomas Birien <thomasbirien@gmail.com>
2017-02-28 16:31:48 +0100

    validation for boat debugged

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,15 +1,9 @@
 class Boat < ApplicationRecord
   has_many :bookings
   belongs_to :user
-  validates :nickname, presence: true
-  validates :brand, presence: true
-  validates :manufacturer, presence: true
-  validates :immatriculation, presence: true, uniqueness: true
-  validates :model, presence: true
-  validates :boat_type, presence: true
-  validates :length, presence: true
-  validates :width, presence: true
-  validates :draught, presence: true
-  validates :multihull, presence: true
+  validates_presence_of :nickname, :brand, :manufacturer, :immatriculation, :boat_type, :length, :width, :draught
+  validates :immatriculation, uniqueness: true
+
+
 
 end
