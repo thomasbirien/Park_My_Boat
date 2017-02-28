e1cc4c515ffabf74b31879b9803564f4beed3e49
Buffin Francois <fbuffin69@gmail.com>
2017-02-28 18:56:24 +0100

    seeds test

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,21 @@
 class Booking < ApplicationRecord
+
+  before_validation :set_invoiced_price
+
   belongs_to :user
   belongs_to :place
   belongs_to :boat
   validates :arrival_date, presence: true
   validates :departure_date, presence: true
   validates :invoiced_price, presence: true
+
+  private
+
+  def set_invoiced_price
+    self.invoiced_price = place.place_price * number_of_days
+  end
+
+  def number_of_days
+    (departure_date - arrival_date).to_i
+  end
 end
