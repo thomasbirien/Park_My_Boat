f93c77266c2ff0c795bfa08c87f9fdff2224fcaa
Thomas Birien <thomasbirien@gmail.com>
2017-02-28 14:55:31 +0100

    Path url: rails routes ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :ports, only: [:index, :show] do
+    resources :bookings, only: [:new, :create, :show]
+  end
+
+  resources :bookings, only: [:index]
+  resources :boats, only: [:index, :new, :create, :edit, :update]
+
+  resource :profile, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
