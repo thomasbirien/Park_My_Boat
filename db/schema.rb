1130332fa94385e88ca74cd2f950f67d1442784e
Buffin Francois <fbuffin69@gmail.com>
2017-03-01 16:49:51 +0100

    Add Modal in home page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170301093437) do
+ActiveRecord::Schema.define(version: 20170301112657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20170301093437) do
     t.integer  "user_id"
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_boats_on_deleted_at", using: :btree
     t.index ["user_id"], name: "index_boats_on_user_id", using: :btree
   end
 
@@ -42,7 +44,9 @@ ActiveRecord::Schema.define(version: 20170301093437) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.integer  "place_id"
+    t.datetime "deleted_at"
     t.index ["boat_id"], name: "index_bookings_on_boat_id", using: :btree
+    t.index ["deleted_at"], name: "index_bookings_on_deleted_at", using: :btree
     t.index ["place_id"], name: "index_bookings_on_place_id", using: :btree
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
   end
@@ -66,6 +70,8 @@ ActiveRecord::Schema.define(version: 20170301093437) do
     t.datetime "created_at",                         null: false
     t.datetime "updated_at",                         null: false
     t.integer  "port_id"
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_places_on_deleted_at", using: :btree
     t.index ["port_id"], name: "index_places_on_port_id", using: :btree
   end
 
@@ -85,6 +91,8 @@ ActiveRecord::Schema.define(version: 20170301093437) do
     t.datetime "updated_at",     null: false
     t.text     "description"
     t.integer  "size"
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_ports_on_deleted_at", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -109,6 +117,8 @@ ActiveRecord::Schema.define(version: 20170301093437) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_users_on_deleted_at", using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
