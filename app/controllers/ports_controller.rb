c762df5e2fe37a3f366367a561bec3a29ea9826b
= <jbcousinat@gmail.com>
2017-03-02 16:41:22 +0100

    enable link to show 3

--- app/controllers/ports_controller.rb
+++ app/controllers/ports_controller.rb
@@ -2,7 +2,6 @@ class PortsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
-
     # display only port with places > 1
     # @ports = Port.all.select { |port| port.places.count >= 1 }
     # display target port
@@ -15,11 +14,26 @@ class PortsController < ApplicationController
       departure_date: params[:departure_date]
     }
 
+    @date_range = (Date.strptime(params[:arrival_date], '%m/%d/%Y')..Date.strptime(params[:departure_date], '%m/%d/%Y'))
+
+
     # @ports = (Port.where("port_name ILIKE ?", "%#{params[:port_name]}%"))
-    @ports_selected = Port.all
-    @ports_selected = @ports_selected.joins(:places).where("places.length > ? AND places.width > ? AND places.draught > ?", params[:length], params[:width], params[:draught])
-    @ports_selected = @ports_selected.joins(:places, :bookings).where("bookings.arrival_date > ? AND bookings.departure_date >= ?", Date.strptime(params[:departure_date], "%m/%d/%Y"), Date.strptime(params[:arrival_date], "%m/%d/%Y"))
+    # @ports_selected = Port.all
+    # @ports_selected = @ports_selected.joins(:places).where("places.length > ? AND places.width > ? AND places.draught > ?", params[:length], params[:width], params[:draught])
+    # @ports_selected = @ports_selected.joins(:bookings).where("bookings.arrival_date > ? AND bookings.departure_date >= ?", Date.strptime(params[:departure_date], "%m/%d/%Y"), Date.strptime(params[:arrival_date], "%m/%d/%Y"))
+    # @ports_selected = @ports_selected.joins(:bookings).select {|port| port.places.available_at(@date_range)}
+
+@places_selected = Place.all
+@places_selected = @places_selected.where("places.length > ? AND places.width > ? AND places.draught > ?", params[:length], params[:width], params[:draught])
+@places_selected = @places_selected.select {|place| place.available_at(@date_range)}
+
+@ports_selected = @places_selected.map do |place|
+  place.port
+end.uniq
+
 binding.pry
+
+#need to set value if places.booking do not exist or is nil present blank
 
 # SELECT * FROM users WHERE name = 'Joe' AND email = 'joe@example.com';
 
