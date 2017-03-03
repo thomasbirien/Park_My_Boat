14bf4404638d995b81542c867a6eeca59c324d46
= <jbcousinat@gmail.com>
2017-03-03 12:04:18 +0100

    setup figaro for gg API key

--- app/models/place.rb
+++ app/models/place.rb
@@ -8,10 +8,16 @@ class Place < ApplicationRecord
   validates :place_price, presence: true
 
   def available_at(date_range)
+  # implentation of date not done
     available = true
       self.bookings.each do |booking|
-
-        available = false if ((booking.arrival_date..booking.departure_date).to_a & @date_range.to_a)
+        if @input_departure_date == booking.arrival_date && (((booking.arrival_date..booking.departure_date).to_a & @date_range.to_a).count == 1)
+          available = true
+        elsif @input_arrival_date == booking.arrival_date && (((booking.arrival_date..booking.departure_date).to_a & @date_range.to_a).count == 1)
+          available = true
+        elsif ((booking.arrival_date..booking.departure_date).to_a & @date_range.to_a)
+        available = false
+        end
       end
     available
   end
