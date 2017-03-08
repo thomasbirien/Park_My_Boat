4187dc21ba88e6e4957c4cc51ece65b7e24e87a3
Thomas Birien <thomasbirien@gmail.com>
2017-03-08 11:57:58 +0100

    need login for booking

--- app/controllers/ports_controller.rb
+++ app/controllers/ports_controller.rb
@@ -1,5 +1,5 @@
 class PortsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     port_filter = PortFilter.new(params)
