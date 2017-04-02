b4189cfae3d4440c96938810082c82a70a2968f3
Thomas Birien <thomasbirien@gmail.com>
2017-04-02 14:46:04 +0200

    sengrid added for production

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.smtp_settings = {
-  user_name: 'your_sendgrid_username',
-  password: 'your_sendgrid_password',
+  user_name: ENV["SENGRID_USERNAME"],
+  password: ENV["SENGRID_PASSWORD"],
   domain: 'yourdomain.com',
   address: 'smtp.sendgrid.net',
   port: 587,
