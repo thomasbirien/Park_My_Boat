4d87224b396d9a3e290cebf8a28a179125d82660
Thomas Birien <thomasbirien@gmail.com>
2017-04-02 17:09:26 +0200

    from added

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,6 +1,5 @@
 class UserNotifier < ActionMailer::Base
   default :from => 'parkmyboat@outlook.fr'
-
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_signup_email(user)
     @user = user
