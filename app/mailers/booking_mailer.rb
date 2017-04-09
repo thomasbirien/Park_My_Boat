73310ed222f989dc42f08ce67262610abc289b6f
Thomas Birien <thomasbirien@gmail.com>
2017-04-09 19:20:10 +0200

    , added

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -3,7 +3,7 @@ class BookingMailer < ApplicationMailer
 
   def capitainerie(booking)
     @booking = booking
-    mail( to: 'parkmyboat@outlook.fr'
+    mail( to: 'parkmyboat@outlook.fr',
       subject: 'une reservation est demandé pour votre port')
   end
 end
